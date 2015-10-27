dec3419a530a64b75ecad23cf277c1581217b895
Bruno Helder <brunohelder@cjr.org.br>
2015-10-27 08:17:57 -0200

    atualizei

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026162928) do
+ActiveRecord::Schema.define(version: 20151027101312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "Nome"
-        t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "activity_types", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string   "job_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "Nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
