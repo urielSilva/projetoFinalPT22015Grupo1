0c1c0390bf3940928aa40b41bd0896b8ea827cb1
Bruno Helder <brunohelder@cjr.org.br>
2015-11-03 13:48:57 -0200

    Eita

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   resources :roles
   resources :jobs
 
-  get 'sitebonito' => 'activity_types#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
